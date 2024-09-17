49a4d6d752e207d34098ff6306fe8fe3907333c3
Matthew Neiderhuber <mniederhuber@unc.edu>
2024-09-17 15:42:33 -0400

    removed docker compose stuff

--- src/dockerRun.sh
+++ src/dockerRun.sh
@@ -1,5 +1,5 @@
-docker run -d -p 8080:8080 \
+docker run -dit -p 8080:8080 \
     -e OPENAI_API_KEY=${OPEN_API_KEY} \
     -e ENTREZ_EMAIL=${ENTREZ_EMAIL} \
     -e ENTREZ_API_KEY=${ENTREZ_API_KEY} \
-    sragent:dev
+    sragent:dev /bin/bash
